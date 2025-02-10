2b56f72d2389e5f5411868b8f4eb9bbc95aeb81d
kangheeyoun <kkhy0304@naver.com>
2025-02-10 14:32:16 +0900

    update

--- src/assets/Products.js
+++ src/assets/Products.js
@@ -1,8 +1,8 @@
 export const products = [
-    {id:1, name: '햄부기', price:20000, image:'./public/images/1.jpg',category:'Best'},
+    {id:1, name: '햄부기', price:20000, image:'./images/1.jpg',category:'Best'},
     {id:2, name: '햄북', price:8000, image:'./public/images/2.jpg',category:'New'},
-    {id:3, name: '햄북스딱스', price:3000, image:'./public/images/3.jpg',category:'Set'},
-    {id:4, name: '함부르크', price:5000, image:'./public/images/4.jpg',category:'Beverage'},
-    {id:5, name: '햄부가우', price:4000, image:'./public/images/5.jpg',category:'Desert'}
+    {id:3, name: '햄북스딱스', price:3000, image:'./images/3.jpg',category:'Set'},
+    {id:4, name: '함부르크', price:5000, image:'./images/4.jpg',category:'Beverage'},
+    {id:5, name: '햄부가우', price:4000, image:'./images/5.jpg',category:'Desert'}
 ];
 export const categories = ['All','Best','New','Set','Beverage','Desert'];
